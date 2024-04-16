a8248c44cc2ab16630374acd6390d973cea79975
CommitTODev <abhishekshivale45@gmail.com>
2024-04-16 21:34:38 +0530

    day 3 complete

---
+++ DAY_3/main/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("Hello, world!");
+    // 1. All variable in Rust Is Unmutable butt we can change with using "mute" afte varible
+    // 2. Comman parctice is to use all upcase latter for using Const Name in Rust
+    // 3. Const Never mut with mut
+    // 4. we can re declare veriable
+
+    //data DATA TYpe
+    // Scalar and Compound
+        // Scaler
+            //Interger :-
+                //Signed is Int is Only Positive
+                //Unsigned is Int is might Positive or Negative
+}
